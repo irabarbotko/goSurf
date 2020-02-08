a299b082c40982c5b079eaff2668a5b12ae58d58
irabarbotko <barbotkoirina@gmail.com>
2020-02-08 22:29:52 +0200

    11

--- app/js/main.js
+++ app/js/main.js
@@ -6,8 +6,13 @@ $('.header__slider').slick({
   fade: true,
   prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
   nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+  asNavFor: '.slider-dots'
 });
 
-
+$('.slider-dots').slick({
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  asNavFor: '.header__slider',
+});
   
 });
